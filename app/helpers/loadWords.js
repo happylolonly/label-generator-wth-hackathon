718b2647abba82a05362016663ce6dc0e3804231
HappyLoL <happylolonly@gmail.com>
2018-05-04 19:46:50 +0300

    fast fix words loader

--- app/helpers/loadWords.js
+++ app/helpers/loadWords.js
@@ -11,20 +11,23 @@ module.exports = () => {
       const vineTitlesArr = data.split('\n');
       const rand = Math.floor(Math.random() * vineTitlesArr.length);
       const baseTitle = vineTitlesArr[rand];
-      const query = `https://paraphraser.ru/api?c=vector&query=${encodeURIComponent(baseTitle)}&lang=ru&type=vector&token=882a9e4abe9c34f3fe8ca0e8479ef64e02b36def&top=5&scores=0&forms=0`;
 
-      axios.get(query)
-        .then(text => {
-          const returnArr = text.data.response[1].vector;
-          if (returnArr.length > 0) {
-            resolve(returnArr);
-            return;
-          }
-          resolve(query);
-        })
-        .catch(err => {
-          reject(err);
-        });
+      resolve(baseTitle.split(' '));
+
+      // const query = `https://paraphraser.ru/api?c=vector&query=${encodeURIComponent(baseTitle)}&lang=ru&type=vector&token=882a9e4abe9c34f3fe8ca0e8479ef64e02b36def&top=5&scores=0&forms=0`;
+
+      // axios.get(query)
+      //   .then(text => {
+      //     const returnArr = text.data.response[1].vector;
+      //     if (returnArr.length > 0) {
+      //       resolve(returnArr);
+      //       return;
+      //     }
+      //     resolve(query);
+      //   })
+      //   .catch(err => {
+      //     reject(err);
+      //   });
     });
   });
 
